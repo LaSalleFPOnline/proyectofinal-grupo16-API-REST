37984e7bf8fe076f0d4da3f025b49c08098604c6
ismael <ismaelreyplata@gmail.com>
2023-04-23 13:13:06 +0200

    filtrar un get de id usuario para mostrar solo las quedadas de un usuario determinado

---
+++ myhangouts.php
@@ -0,0 +1,33 @@
+<?php
+//controlar el acceso a la api rest
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+include "config.php";
+include "utils.php";
+$dbConn =  connect($db);
+
+
+/*
+  listar todos los posts o solo uno
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['id_user']))
+    {
+      //Mostrar un post
+      
+      $sql = $dbConn->prepare("SELECT title_hangout, image_hangout, description_hangout FROM hangouts
+      INNER JOIN targeted ON hangouts.id_hangout = targeted.id_hangout
+      WHERE id_user=:id_user");
+      //$sql = $dbConn->prepare("SELECT * FROM hangouts where id_hangout=:id_hangout");
+      $sql->bindValue(':id_user', $_GET['id_user']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetchAll()  );
+      exit();
+	}
+}
+?>
