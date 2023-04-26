bc24a912aa93456d22cf407c1616d3c1b102f3f5
ismael <ismaelreyplata@gmail.com>
2023-04-26 21:15:00 +0200

    formulario crear nueva quedada, desde angular

--- newhangout.php
+++ newhangout.php
@@ -18,22 +18,23 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     
     //lo recibe por parametros
     //$input =  $_POST;
-
+    $input = file_get_contents('php://input');
+    $data = json_decode($input);
     //recibe por json
-    $data = json_decode(file_get_contents("php://input"));
+    //$data = json_decode(file_get_contents("php://input"));
     
     $sql = "INSERT INTO hangouts (title_hangout, city_hangout, image_hangout, description_hangout, data_hangout, hour_hangout, difficult_hangout) VALUES (:title_hangout, :city_hangout, :image_hangout, :description_hangout, :data_hangout, :hour_hangout, :difficult_hangout)";
     $stmt = $dbConn->prepare($sql);
 
-    $f = $data->data_hangout;
+    //$f = $data->data_hangout;
 
-    $fecha = DateTime::createFromFormat('d/m/Y', $f);
+    //$fecha = DateTime::createFromFormat('d/m/Y', $f);
     
     $stmt->bindValue(':title_hangout', $data->title_hangout);
     $stmt->bindValue(':city_hangout', $data->city_hangout);
     $stmt->bindValue(':image_hangout', $data->image_hangout);
     $stmt->bindValue(':description_hangout', $data->description_hangout);
-    $stmt->bindValue(':data_hangout', $fecha->format('Y-m-d'));
+    $stmt->bindValue(':data_hangout', $data->data_hangout);
     $stmt->bindValue(':hour_hangout', $data->hour_hangout);
     $stmt->bindValue(':difficult_hangout', $data->difficult_hangout);
     
